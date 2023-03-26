8583d697e5682976e41fa25550ed665362898b9d
kengo <mathematics9973@gmail.com>
2023-03-27 00:40:19 +0900

    friendship

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,10 +14,28 @@ class User < ApplicationRecord
   # has_many :teams, through: :team_members  # 機能拡張時に使うかも
   # has_many :owned_teams, class_name: 'Team', foreign_key: 'owner_id', dependent: :destroy
   # ↑機能拡張時に使うかも
-  
+  has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
   validates :name, presence: true, length: { maximum: 30}
   validates :email, presence: true, length: { maximum: 255}, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password, length: { minimum: 6 }
+
+  #指定のユーザをフォローする
+  def follow!(other_user)
+    active_relationships.create!(followed_id: other_user.id)
+  end
+
+  #フォローしているかどうかを確認する
+  def following?(other_user)
+    active_relationships.find_by(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
   # has_secure_password
   #上の記述はいらないのでコメントアウト。deviseでencrypt_passwordを使用しているので。
 end
