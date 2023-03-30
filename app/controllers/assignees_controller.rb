1885bfe9b91d912d62ee74af5f4d44356247ff1c
kengo <mathematics9973@gmail.com>
2023-03-30 16:37:16 +0900

    assignees_view

--- app/controllers/assignees_controller.rb
+++ app/controllers/assignees_controller.rb
@@ -1,70 +1,43 @@
 class AssigneesController < ApplicationController
   before_action :set_assignee, only: %i(new create edit update destroy)
 
-  # GET /assignees or /assignees.json
   def index
     @assignees = Assignee.all.includes(:user).order(created_at: :desc).page(params[:page])
   end
 
-  # GET /assignees/1 or /assignees/1.json
   def show
   end
 
-  # GET /assignees/new
   def new
     @assignee = Assignee.new
   end
 
-  # GET /assignees/1/edit
   def edit
   end
 
-  # POST /assignees or /assignees.json
   def create
-    @assignee = Assignee.new(assignee_params)
-
-    respond_to do |format|
-      if @assignee.save
-        format.html { redirect_to assignee_url(@assignee), notice: "Assignee was successfully created." }
-        format.json { render :show, status: :created, location: @assignee }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @assignee.errors, status: :unprocessable_entity }
-      end
-    end
+    user = User.find(params[:user_id])
+    @issue.assignees.create(user: user)
+    redirect_to @issue
   end
 
-  # PATCH/PUT /assignees/1 or /assignees/1.json
-  def update
-    respond_to do |format|
-      if @assignee.update(assignee_params)
-        format.html { redirect_to assignee_url(@assignee), notice: "Assignee was successfully updated." }
-        format.json { render :show, status: :ok, location: @assignee }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @assignee.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /assignees/1 or /assignees/1.json
   def destroy
-    @assignee.destroy
-
-    respond_to do |format|
-      format.html { redirect_to assignees_url, notice: "Assignee was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    user = User.find(params[:user_id])
+    assignee = @issue.assignees.find_by(user: user)
+    assignee.destroy if assignee
+    redirect_to @issue
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_assignee
-      @assignee = Assignee.find(params[:id])
-    end
+  def set_assignee
+    @assignee = Assignee.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def assignee_params
-      params.require(:assignee).permit(:user_id, :issue_id)
-    end
+  def set_issue
+    @issue = Issue.find(params[:issue_id])
+  end
+
+  def assignee_params
+    params.require(:assignee).permit(:user_id, :issue_id)
+  end
 end
