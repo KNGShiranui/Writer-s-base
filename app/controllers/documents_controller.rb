889261f34519110b070d086d3dbc77d2883ee272
kengo <mathematics9973@gmail.com>
2023-04-03 21:50:28 +0900

    document_contrpller_and_view

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -8,26 +8,33 @@ class DocumentsController < ApplicationController
     # @documents = Document.all.includes(:user).order(created_at: :desc).page(params[:page])
     @documents = @branch.documents.order(created_at: :desc).page(params[:page])
   end
-
+  
   def show
+    
+    @document = Document.find(params[:id])
+    @repository = params[:repository_id] 
+    @branch = params[:branch_id]
   end
-
+  
   def new
+    # binding.pry
     @repository = Repository.find(params[:repository_id])
     @branch = Branch.find(params[:branch_id])
-    @document = Document.new
-    # binding.pry
+    @document = @branch.documents.build
   end
 
   def edit
+    @repository = Repository.find(params[:repository_id])
+    @branch = Branch.find(params[:branch_id])
   end
 
   def create
     @document = Document.new(document_params)
-
+    @repository = Repository.find(params[:document][:repository_id])
+    @branch = Branch.find(params[:document][:branch_id])
     respond_to do |format|
       if @document.save
-        format.html { redirect_to document_url(@document), notice: "Document was successfully created." }
+        format.html { redirect_to document_url(@document, repository_id: @repository.id, branch_id: @branch.id), notice: "Document was successfully created." }
         format.json { render :show, status: :created, location: @document }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -37,9 +44,12 @@ class DocumentsController < ApplicationController
   end
 
   def update
+    # binding.pry
+    @repository = Repository.find(params[:document][:repository_id])
+    @branch = Branch.find(params[:document][:branch_id])
     respond_to do |format|
       if @document.update(document_params)
-        format.html { redirect_to document_url(@document), notice: "Document was successfully updated." }
+        format.html { redirect_to document_url(@document, repository_id: @repository.id, branch_id: @branch.id), notice: "Document was successfully updated." }
         format.json { render :show, status: :ok, location: @document }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -49,10 +59,12 @@ class DocumentsController < ApplicationController
   end
 
   def destroy
+    @repository = Repository.find(params[:repository_id])
+    @branch = Branch.find(params[:branch_id])
     @document.destroy
 
     respond_to do |format|
-      format.html { redirect_to documents_url, notice: "Document was successfully destroyed." }
+      format.html { redirect_to documents_path(repository_id: @repository.id, branch_id: @branch.id), notice: "Document was successfully destroyed." }
       format.json { head :no_content }
     end
   end
@@ -63,6 +75,6 @@ class DocumentsController < ApplicationController
   end
 
   def document_params
-    params.require(:document).permit(:name, :content, :user_id, :branch_id, :repository_id)
+    params.require(:document).permit(:name, :content, :user_id, :branch_id)
   end
 end
