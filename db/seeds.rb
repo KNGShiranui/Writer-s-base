ecb2fafd192769a9680dcd5b9fd5b43bf4d10b06
kengo <mathematics9973@gmail.com>
2023-04-05 00:35:20 +0900

    create_seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+# <administratorデータ>
+User.create!(
+  name: "KNG1", 
+  email: 'KNG1@example.com', 
+  password: '11101252', 
+  password_confirmation: '11101252', 
+  admin: true)
+  
+# <userデータ>
+10.times do |i|
+  User.create!(
+    name: "KNG#{i + 2}", 
+    email: "KNG#{i + 2}@example.com", 
+    password: '11101252', 
+    password_confirmation: '11101252')
+end
+
+users = User.all
+users = users.map{|user| user.id}
+start_day = DateTime.new(2023, 1, 5, 0, 0, 0)
+last_day = DateTime.new(2023, 3, 15, 23, 59, 59)
+
+# <repositoryデータ>
+10.times do |i|
+  Repository.create!(
+    name: "リポジトリ#{i + 1}",
+    description: "ジャスティス#{i + 1}",
+    user_id: users.sample)
+end
