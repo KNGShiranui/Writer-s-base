10abe722f5582cef4a6783533c217e7453db1062
kengo <mathematics9973@gmail.com>
2023-04-11 03:35:32 +0900

    access_setting いろいろと大苦戦。コメント参照

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -15,30 +15,38 @@ class Ability
       can :access, :rails_admin
       can :manage, :all
     end
-    ## リポジトリ
-    can [:create, :read], Repository
-    can :update, Repository, user_id: user.id
-    can :destroy, Repository, user_id: user.id
-  
-    ## ブランチ
+
+    ## assigneeについて（なくていいかも）
+
+    ## branchについて
     can [:read], Branch
+    ## TODO:現状、branchはrepositoryの持ち主しか切ることができない。
+    # 今後、「チームの一員ならば切れる」「公開されていて誰にでも切れる」なども選択できるようにすること
     can [:create_from_existing, :update], Branch, repository: { user_id: user.id }
     can :destroy, Branch do |branch|
       branch.repository.user_id == user.id || branch.user_id == user.id
     end
+
+    ## commitについて
+
+    ## conversationについて
+    # かえってややこしいのでconversations_controllerで制御。
   
-    ## ドキュメント
-    # binding.pry
+    ## documentについて
     can :read, Document
-    can [:create, :update], Document, branch: { user_id: user.id }
-    can [:destroy], Document do |document|
-      document.branch.user_id == user.id
+    can :create, Document do |document|
+      repository = Repository.find_by(id: repository_id)
+      document.new_record? && branch.user_id == user.id || document.new_record? && repository.user_id == user.id
     end
+    can [:update, :destroy], Document, branch: { user_id: user.id }
       ## 以下はブランチ作成者以外にもブランチ作成者が許可を出した場合も可能とするという手順を仕様とし始めたもの
       # 2023.4.10現在は不採用
       # document.branch.user_id == user.id || user.allowed_users.include?(document.user)
 
-    ## イシュー
+    ## favorite_repositoryについて
+    # ネストしていないindexビューのみなので、ログインしているか否かでなりすましを弾ける
+
+    ## issueについて
     can :read, Issue
     can :create, Issue do |issue|
       repository = Repository.find_by(id: repository_id)
@@ -47,7 +55,17 @@ class Ability
       issue.new_record? && repository.user_id == user.id
     end
     can [:update, :destroy], Issue, repository: { user_id: user.id }
-    ## コミット
+    
+    ## messageについて
+    # かえってややこしいのでconversations_controllerで制御。
+
+    ## relationshipについて
+# 。とりあえず今はなくてOK
+
+    ## repositoryについて
+    can [:create, :read], Repository
+    can :update, Repository, user_id: user.id
+    can :destroy, Repository, user_id: user.id  
   end
   ## 以下参考
   # class Ability
