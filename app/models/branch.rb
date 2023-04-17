8bd78bfcf4f470dda149d4b9342cf754fd3726c9
kengo <mathematics9973@gmail.com>
2023-04-18 02:55:50 +0900

    I18T

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,7 +1,7 @@
 class Branch < ApplicationRecord
   belongs_to :repository
   # belongs_to :parent, class_name: "Branch", optional: true
-  has_many :commits
+  has_many :commits, dependent: :destroy
   has_many :documents, dependent: :destroy
   # has_many :objects # 機能拡張時に使うかも
   belongs_to :user
