b76c193b0c391ac99eae3035a5c34404eef363dc
kengo <mathematics9973@gmail.com>
2023-04-19 10:22:13 +0900

    diff_arrange

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,20 +11,22 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     ## TODO:↓のコードを有効にすると、ログインボーナスは一日一回という条件が付けられる
-    if resource.last_login_bonus_date.nil? || resource.last_login_bonus_date.nil < Date.today
+    if resource.last_login_bonus_date.nil? || resource.last_login_bonus_date < Date.today
       # ランダムなログインボーナスを生成
       login_bonus = generate_login_bonus
       # ユーザーのポイントにログインボーナスを追加
-      resource.update(points: resource.points + login_bonus)
+      resource.update(points: resource.points + login_bonus, last_login_bonus_date: Date.today)
       # ログインボーナスを通知
       flash[:success] = "ログインボーナス#{login_bonus} ポイント獲得！"
       # リダイレクト先を設定 (例: ユーザープロフィールページ)
-      user_path(current_user) # ユーザーページにリダイレクト
+      # user_path(current_user) # ユーザーページにリダイレクト。FIXME:ここにあると、
+      # ifの条件に合致しない場合にnilになってリダイレクトされなくなる
     end
+    user_path(current_user) 
   end
 
   def generate_login_bonus
-    # ランダムなログインボーナスを 1 から 5 の範囲で生成
+    # ランダムなログインボーナスを 1 から 5 の範囲で生成  
     rand(1..5)
   end
 
