2f4d334173a7b10d0fc4ee75ee0e5f3256e0168a
kengo <mathematics9973@gmail.com>
2023-04-25 11:49:46 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,14 @@
 Rails.application.routes.draw do
   root 'top#index'
-  ## deviseを使う場合のゲストログイン実装には以下の記載が必要
+
   devise_for :users, controllers: {
     sessions: 'sessions',
-    # ↓deviseで更新しようとすると、なぜかupdateがdestroyになる・・・
-    # registrations: 'users/registrations' # user#update用ルーティング
-    # ↑users_controllerで制御することにしたので使わない
-    # registrations: 'users' # user#update用ルーティング
   }
-  ## deviseを使う場合は以下のようにブロックでゲストログインルーティングの記載をする必要あり
   devise_scope :user do
     get 'guest_sign_in', to: 'sessions#guest_sign_in'
     get 'guest_admin_sign_in', to: 'sessions#guest_admin_sign_in'
-    get 'users/edit', to: 'users#edit' # users_controllerでupdateを制御することにしたので追記
-    put 'users', to: 'users#update' # users_controllerでupdateを制御することにしたので追記
+    get 'users/edit', to: 'users#edit'
+    put 'users', to: 'users#update'
   end
 
   ## top
@@ -27,10 +22,6 @@ Rails.application.routes.draw do
   get 'game/painting', to: 'game#painting'
 
   resources :assignees
-  ## newアクションは使わないので書き換え。
-  # resources :branches do
-  #   post 'create_from_existing', on: :member
-  # end
   resources :branches do
     post 'create_from_existing', on: :member
   end  
@@ -53,7 +44,7 @@ Rails.application.routes.draw do
       get :followers 
     end
   end
-  resources :points, only: %i(new create) # point送信用
+  resources :points, only: %i(new create)
   resources :repositories do
     collection do
       get :search
@@ -61,7 +52,7 @@ Rails.application.routes.draw do
   end
   resources :users, only: %i(index show update)
   resources :favorite_repositories, only: %i(index create destroy)
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
